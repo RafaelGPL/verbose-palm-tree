60353a73df3ce587072f4b680e7855227a4f0ada
Rafael GPL <rafael.gpl@dashws.com>
2025-03-20 17:12:57 -0700

    First commit

---
+++ posts/views.py
@@ -0,0 +1,35 @@
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView
+)
+from .models import Post
+from django.urls import reverse_lazy
+
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+class PostCreateView(CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ["title", "subtitle", "body", "author", "status"]
+
+class PostUpdateView(UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = [
+        "title", "subtitle", "body", "status"
+    ]
+
+class PostDeleteView(DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy("list")      # Update this if your ListView isn't mapped to "list"
